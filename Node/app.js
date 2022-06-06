7a49a428f994fa393b54b5fed93d07bd4f2e2db0
Ollie Bull <59794969+OllieBullGB@users.noreply.github.com>
2022-06-06 11:55:05 +0100

    Node boilerplate

---
+++ Node/app.js
@@ -0,0 +1,54 @@
+/**
+ *  Filename: app.js
+ *  
+ *  Author: Ollie Bull
+ * 
+ *  Date: 20th October 2021
+ * 
+ *  Description: The backend server in charge of processing webpage access requests
+ *               aswell as email sign-up requests for Assignment 2 of CMP-4011A
+ *               web-based development module at UEA
+ * 
+ *  Version: 20/10/21 v1.0, 08/11/21 v1.1
+ */
+
+const express = require('express');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const {
+    check,
+    validationResult
+} = require('express-validator');
+//Import routers
+const siteRoutes = require('./routes/site');
+
+//create an express web server on Localhost:3000
+const app = express();
+
+//Forcing the app to use a package which protects from well known
+//web vulnerabilities by setting appropriate HTTP headers
+app.use(helmet());
+
+//Forces the app to remove any express branding to help prevent
+//targeted attacks to the webserver
+app.disable('x-powered-by');
+
+//give express static access to the folders used to store the webpages aswell as
+//associated CSS, client side JS and images
+app.use(express.static('public'));
+app.use('/css', express.static('/css'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/img', express.static(__dirname + '/img'));
+
+//When a non-post request is recieved by the server, process it as a GET request
+//Using pre-determined routes
+app.get('/', (req, res, next) => {
+    res.status(200);
+    res.render('index', {
+        title: 'Homepage',
+        css: 'index'
+    });
+});
+app.use('/site', siteRoutes);
+
+module.exports = app;
