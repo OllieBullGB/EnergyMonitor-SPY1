c68f20e8fd9fa0f405052e9801ab615fe17e8780
Ollie Bull <olliebull150@gmail.com>
2022-06-07 15:44:19 +0100

    seperate input functions from into own file

---
+++ Code/js/inputManager.js
@@ -0,0 +1,103 @@
+function addNumber(element) 
+{
+  let selectedElement = document.getElementsByClassName("selected");
+  selectedElement = selectedElement[0];
+  selectedElement.value = selectedElement.value+element.value;
+}
+
+function switchSelection(elementName)
+{
+  let currentSelectedElement = document.getElementsByClassName("selected");
+  currentSelectedElement[0].classList.remove("selected");
+  
+  let newElement = document.getElementById(elementName);
+  newElement.classList.add("selected");
+}
+
+function resetInput() 
+{
+  let elements = document.getElementsByClassName("selected");
+  let selectedElement = elements[0];
+  selectedElement.value = selectedElement.value.slice(0, -1);
+}
+
+function loadSavedInputs()
+{
+  let usage = LocalDataManager.getUsage().usage;
+  let panelInfo = LocalDataManager.getPanelInfo();
+
+  if(typeof(document.getElementById("update-usage")) != 'undefined' && !isNaN(usage))
+  {
+    document.getElementById("current").innerHTML = 
+    `
+    <var> ${usage} </var>
+    <p>
+      kWh
+      <span class="material-symbols-outlined">
+        bolt
+      </span>
+    </p>
+    `
+  }
+  else if(typeof(document.getElementById("update-usage")) != 'undefined')
+  {
+    document.getElementById("current").innerHTML = 
+    `
+    <var> 0 </var>
+    <p>
+      kWh
+      <span class="material-symbols-outlined">
+        bolt
+      </span>
+    </p>
+    `
+  }
+
+  if(typeof(document.getElementById("update-area")) != 'undefined')
+  {
+    if(!isNaN(panelInfo.area) && panelInfo.area > 0)
+    {
+      document.getElementById("update-area").value = panelInfo.area;
+    }
+    if(!isNaN(panelInfo.angle) && panelInfo.angle > 0)
+    {
+      document.getElementById("update-angle").value = panelInfo.angle;
+    }
+    if(!isNaN(panelInfo.direction) && panelInfo.direction > 0)
+    {
+      document.getElementById("update-direction").value = panelInfo.direction;
+    }
+  }
+}
+
+loadSavedInputs();
+
+function updateUsage()
+{
+  let usage = parseFloat(document.getElementById("usage-update").value);
+  LocalDataManager.setUsage(usage);
+  loadSavedInputs();
+}
+
+function updateArea()
+{
+  let area = parseFloat(document.getElementById("update-area").value);
+  LocalDataManager.setArea(area);
+  switchSelection("update-angle");
+  loadSavedInputs();
+}
+
+function updateAngle()
+{
+  let angle = parseFloat(document.getElementById("update-angle").value);
+  LocalDataManager.setAngle(angle);
+  switchSelection("update-direction");
+  loadSavedInputs();
+}
+
+function updateDirection()
+{
+  let direction = parseFloat(document.getElementById("update-direction").value);
+  LocalDataManager.setDirection(direction);
+  window.location.href="daily.html";
+}
