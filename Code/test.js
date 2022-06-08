2ad2659dc0a6101fae7ccd1119860df56fe53b8d
SD449 <52426901+SD449@users.noreply.github.com>
2022-06-08 22:50:05 +0100

    formatting

--- Code/test.js
+++ Code/test.js
@@ -19,16 +19,16 @@ const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"
 function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
-		localStorage.setItem('theme', 'dark'); //add this
+		localStorage.setItem('theme', 'dark');
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light');
-		localStorage.setItem('theme', 'light'); //add this
+		localStorage.setItem('theme', 'light');
     }    
 }
 
 if(toggleSwitch) {
-toggleSwitch.addEventListener('change', switchTheme, false);
+	toggleSwitch.addEventListener('change', switchTheme, false);
 }
 
 const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
