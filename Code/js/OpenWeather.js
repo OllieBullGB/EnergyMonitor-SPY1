fccc4fac574f33ca00615b356af6611b7efd8ffd
Ollie Bull <59794969+OllieBullGB@users.noreply.github.com>
2022-06-08 12:17:10 +0100

    attempting to fetch data inside constructor

--- Code/js/OpenWeather.js
+++ Code/js/OpenWeather.js
@@ -1,15 +1,27 @@
 class OpenWeather 
 {
-    constructor(latitude, longitude, apiKey)
+    weatherPoints;
+
+    constructor(latitude, longitude, apiKey, weatherPoints)
     {
         this.latitude = latitude;
         this.longitude = longitude;
         this.apiKey = apiKey;
+        this.weatherPoints = this.init();
+    }
 
-        let url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
-        const res = await axios(url);
-        console.table(res);
-
+    async init()
+    {
+        let data;
+        let url = `https://api.openweathermap.org/data/2.5/forecast?lat=${this.latitude}&lon=${this.longitude}&appid=${this.apiKey}`;
+        const response = fetch(url)
+        .then(res => res.json())
+        .then(data => 
+        {
+            console.table(data);
+            return data;
+        })
+        .catch(err => console.log(err));
     }
 
     getHourlyWeather(day) 
