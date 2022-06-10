06711ee4f8af30ae8bedf5075d4b73d1a94692ec
Ollie Bull <olliebull150@gmail.com>
2022-06-10 16:39:37 +0100

    Update test.js

--- Node/public/test.js
+++ Node/public/test.js
@@ -2,9 +2,11 @@
 function myFunction() 
 {
   var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
+  if (x.style.display === "block") 
+  {
     x.style.display = "none";
-  } else {
+  } else 
+  {
     x.style.display = "block";
   }
 }
